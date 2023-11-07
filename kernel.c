b5c308c4966c24e0d213f9e33735e3262e29eaca
Teknicallity <joshua.s.3322@gmail.com>
2023-11-06 22:50:18 -0500

    Step 1 in progress

--- kernel.c
+++ kernel.c
@@ -78,7 +78,7 @@ void readString(char input[]){
     interrupt(0x10, 0xe * 256 + 0xd, 0, 0, 0); //final carriage return
 }
 
-void readSector(char* buffer, int sector){
+void readSector(char *buffer, int sector){
     int AH = 2; //(this number tells the BIOS to read a sector as opposed to write)
     int AL = 1; //number of sectors to read (use 1)
     char *BX = buffer; //address where the data should be stored to (pass your char* array here)
@@ -94,6 +94,54 @@ void readSector(char* buffer, int sector){
     interrupt(0x13, AX, BX, CX, DX);
 }
 
+readFile(char *fileName, char *buffer, int *sectorsRead ){
+/*
+ * 2. Go through the directory trying to match the file name.
+ If you do not find it, set the number of sectors read to 0 and return.
+
+3. Using the sector numbers in the directory, load the file, sector by sector, into the buffer array.
+ You should add 512 to the buffer address every time you call readSector.
+ Make sure to increment the number of sectors read as you go.
+ */
+    char directory[512];
+    int entrySize = 32;
+
+    readSector(directory, 2); //try sector 2?
+
+    int fileEntry; //current entry position
+    for (fileEntry=0; fileEntry<512; fileEntry+32){
+
+        int match = 0;
+
+        int j;
+        for (j=0; j<6; j++){ //loop through six characters of entry
+            if(fileName[j] == directory[fileEntry + j]){ //filename char, entry offset plus current char
+                match = 1;
+                break;
+            }
+        }
+
+        if (match){
+            int sector = directory[fileEntry + 6];
+            //use another for-loop to load it.
+            // Call readSector using dir[fileentry+6] as the sector number.
+            // Add 512 to the buffer address.
+            // Call readSector with dir[fileentry+7].
+            // Repeat this until you reach a sector number 0.
+
+            *sectorsRead = 0;
+            while(sector != 0){
+                readSector(buffer, sector);
+                buffer+=512;
+                *sectorsRead+=1;
+                sector = directory[fileEntry + 7];
+            }
+
+        }
+
+    }
+}
+
 void handleInterrupt21(int ax, int bx, int cx, int dx){
     if (ax==0){
         printString(bx);
@@ -103,6 +151,9 @@ void handleInterrupt21(int ax, int bx, int cx, int dx){
     }
     if (ax==2){
         readSector(bx, cx);
+    }
+    if (ax==3){
+        readFile(bx, cx, dx);
     }
     if (ax > 2){
         printString("error");
